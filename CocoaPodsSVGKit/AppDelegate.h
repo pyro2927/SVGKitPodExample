690764fbb76977bf861f4b34ba209bece066d9e4
pyro2927 <joseph@pintozzi.com>
2014-01-19 10:12:17 -0600

    Initial commit

---
+++ CocoaPodsSVGKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CocoaPodsSVGKit
+//
+//  Created by Joseph Pintozzi on 1/19/14.
+//  Copyright (c) 2014 pintozzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, "6.0"
+
+target "CocoaPodsSVGKit" do
+  pod 'SVGKit', :git => "https://github.com/pyro2927/SVGKit.git", :branch => "podspec"
+end
+
+target "CocoaPodsSVGKitTests" do
+
+end
+
